9734c7355f05cfbe57806e3397febe82317c15dd
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-09-22 14:55:43 -0500

    Creacion de Footer y header

--- ProteccionCivilWebApp/FE-webApp/src/app/app.component.ts
+++ ProteccionCivilWebApp/FE-webApp/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   public forecasts?: WeatherForecast[];
 
   constructor(http: HttpClient) {
--- ProteccionCivilWebApp/FE-webApp/src/app/app.module.ts
+++ ProteccionCivilWebApp/FE-webApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
