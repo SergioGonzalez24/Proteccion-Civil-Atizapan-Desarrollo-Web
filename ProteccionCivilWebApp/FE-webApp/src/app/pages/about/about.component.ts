92b67f64ed1c2130cb06a4e5de62b4f1945800d9
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-10-12 00:55:42 -0500

    añadir mapa servicios

--- ProteccionCivilWebApp/FE-webApp/src/app/pages/about/about.component.ts
+++ ProteccionCivilWebApp/FE-webApp/src/app/pages/about/about.component.ts
@@ -6,32 +6,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
- 
+  public ContactosData: any;
 
-  directoriosData: any;
-
-  heder = new HttpHeaders({
+  directorioHeader = new HttpHeaders({
     "Access-Control-Allow-Origin":  "*",
     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
     "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, OPTIONS"
-
-
   });
 
   constructor(private http: HttpClient) {
-    http.get('https://jwtauth-webapi.azurewebsites.net/api/directorio/showall',
-    {headers: this.heder }).subscribe((response) => {
-      console.log(response);
-    });
+
+    
   }
 
   ngOnInit(): void {
+    this.getContactosData();
   }
 
+  getContactosData() {
+    this.http.get('https://jwtauth-webapi.azurewebsites.net/api/directorio/showall',
+    { headers: this.directorioHeader }).subscribe(data => {
+      this.setContactoData(data);
+    });
+  }
+
+  setContactoData(data: any) {
+    this.ContactosData = data;
+    this.ContactosData.id = this.ContactosData[0].id;
+    this.ContactosData.departamento = this.ContactosData[0].departamento;
+    this.ContactosData.telefono = this.ContactosData[0].contacto;
+  }
 
 
 
 }
+
+
 
